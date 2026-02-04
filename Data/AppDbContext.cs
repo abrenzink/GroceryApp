0d7785af38b3b876d26da2bcba82a7fd8a17d180
abrenzink <a.brenzink@gmail.com>
2026-02-04 19:03:50 -0300

    feat: adding database connection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    // public DbSet<User> Users => Set<User>();
+    // public DbSet<GroceryItem> GroceryItems => Set<GroceryItem>();
+    // public DbSet<ShoppingCart> ShoppingCarts => Set<ShoppingCart>();
+    // public DbSet<CartItem> CartItems => Set<CartItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // modelBuilder.Entity<User>().ToTable("users");
+        // modelBuilder.Entity<GroceryItem>().ToTable("grocery_items");
+        // modelBuilder.Entity<ShoppingCart>().ToTable("shopping_carts");
+        // modelBuilder.Entity<CartItem>().ToTable("cart_items");
+    }
+}
